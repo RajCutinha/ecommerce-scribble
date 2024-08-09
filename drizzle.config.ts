7fb1406b620c12ef9fb644c141068a9cc3e37a12
Raj Cutinha <rajath.cutinha@protonmail.com>
2024-08-09 21:45:42 +0200

    chore: set up authentication with NextAuth and Drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import { defineConfig } from 'drizzle-kit';
+
+dotenv.config({
+    path: '.env.local'
+});
+
+export default defineConfig({
+    schema: './server/schema.ts',
+    out: './server/migrations',
+    dialect: 'postgresql',
+    dbCredentials: {
+        url: process.env.POSTGRES_DATABASE
+    }
+});
--- environment.ts
+++ environment.ts
@@ -1,7 +1,12 @@
 import { z } from "zod";
 
 export const envSchema = z.object({
-    POSTGRES_DATABASE: z.string()
+    POSTGRES_DATABASE: z.string(),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
+    GITHUB_CLIENT_ID: z.string(),
+    GITHUB_CLIENT_SECRET: z.string(),
+    AUTH_SECRET: z.string()
 });
 
 envSchema.parse(process.env);
